e9e00498232b1d04cd951087c062cca1d631510d
IUT BLAGNAC <wa-il.gana@etu.univ-tlse2.fr>
2025-01-13 09:20:32 +0100

    TP4

--- cucumber_demo/src/test/java/dojo/RunCucumberTest.java
+++ cucumber_demo/src/test/java/dojo/RunCucumberTest.java
@@ -2,11 +2,10 @@ package dojo;
 
 import org.junit.runner.RunWith;
 
+import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)  
+@CucumberOptions(plugin = {"json:target/cucumber.json"} )
 public class RunCucumberTest {
-
-    
 }
-
